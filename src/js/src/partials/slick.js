726472de72d53fc45f9951724b90329ed93f27c8
Steven Grimaldo <srgrimaldo@me.com>
2016-06-23 21:10:14 -0700

    more responsive code

--- src/js/src/partials/slick.js
+++ src/js/src/partials/slick.js
@@ -12,7 +12,23 @@ module.exports = function() {
     $(this).slick({
       slide: '.carousel__item',
       slidesToShow: 4,
-      adaptiveHeight: true
+      adaptiveHeight: true,
+      responsive: [{
+        breakpoint: 1361,
+        settings: {
+          slidesToShow: 3
+        }
+      }, {
+        breakpoint: 1046,
+        settings: {
+          slidesToShow: 2
+        }
+      }, {
+        breakpoint: 736,
+        settings: {
+          slidesToShow: 1
+        }
+      }]
     });
   });
 };
