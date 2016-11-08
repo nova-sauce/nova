a7974630dd5f50e1f06718098e369063f7e61b61
Steven Grimaldo <srgrimaldo@me.com>
2016-11-07 22:06:02 -0800

    adding new tpl changes

--- drupal/sites/all/themes/nova/templates/views/views-view-fields--attachment.tpl.php
+++ drupal/sites/all/themes/nova/templates/views/views-view-fields--attachment.tpl.php
@@ -2,14 +2,14 @@
   $str = $fields['field_category']->content;
   $str = strtolower($str);
 ?>
-<div class="feed__article feed__article--featured feed__article--<?php print $str; ?>" data-key="sameHeights">
+<div class="feed__article feed__article--<?php print $str; ?> feed__article--featured" data-key="sameHeights">
   <div class="article__meta">
     <span class="article__meta__category"><?php print $fields['field_category']->content; ?> <?php print $fields['type']->content; ?></span>
     <span class="article__meta__date"><?php print $fields['created']->content; ?></span>
   </div>
   <div class="feed__article__container">
     <figure class="feed__article__image">
-      <a href="" class="square-pattern">
+      <a href="<?php print $fields['path']->content; ?>" class="square-pattern">
         <div class="image" style="background-image: url('<?php print $fields['field_image']->content; ?>');"></div>
       </a>
     </figure>
@@ -17,9 +17,9 @@
       <div class="feed__article__details__content feed__article__details__content--social">
         <div class="label">Share:</div>
         <ul class="list list--social">
-          <li><a href="http://www.facebook.com/sharer.php?u=http://www.ign.com/articles/2016/09/04/pax-west-2016-gears-of-war-4-we-really-really-missed-horde-mode" target="_blank"><img src="/<?php print IMG_PATH; ?>facebook-circle.png" alt="" /></a></li>
-          <li><a href="http://twitter.com/share?url=http://localhost:3000&amp;text=Uncharted 4: A Thief's End&amp;hashtags=novasauce"><img src="/<?php print IMG_PATH; ?>twitter-circle.png" alt="" /></a></li>
-          <li><a href="https://plus.google.com/share?url=http://www.ign.com/articles/2016/09/04/pax-west-2016-gears-of-war-4-we-really-really-missed-horde-mode" target="_blank"><img src="/<?php print IMG_PATH; ?>google-plus-circle.png" alt="" /></a></li>
+          <li><a href="http://www.facebook.com/sharer.php?u=<?php print $fields['path']->content; ?>" target="_blank"><img src="/<?php print IMG_PATH; ?>facebook-circle.png" alt="" /></a></li>
+          <li><a href="http://twitter.com/share?url=<?php print $fields['path']->content; ?>&amp;text=<?php print $fields['title']->content; ?>&amp;hashtags=novasauce"><img src="/<?php print IMG_PATH; ?>twitter-circle.png" alt="" /></a></li>
+          <li><a href="https://plus.google.com/share?url=<?php print $fields['path']->content; ?>" target="_blank"><img src="/<?php print IMG_PATH; ?>google-plus-circle.png" alt="" /></a></li>
         </ul>
       </div>
       <div class="feed__article__details__content feed__article__details__content--tags">
@@ -35,7 +35,9 @@
         </div>
       </div>
       <div class="feed__article__content__summary">
-        <h3 class="title"><?php print $fields['title']->content; ?></h3>
+        <h3 class="title">
+          <a href="<?php print $fields['path']->content; ?>"><?php print $fields['title']->content; ?></a>
+        </h3>
         <p class="text"><?php print $fields['body']->content; ?></p>
       </div>
     </main>
