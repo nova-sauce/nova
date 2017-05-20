ec0396bb7f5887006123e68660c0df4f090da184
Steven Grimaldo <srgrimaldo@me.com>
2017-05-20 14:54:01 -0500

    updating theme

--- drupal/sites/all/themes/nova/templates/defauts/page--landing-page.tpl.php
+++ drupal/sites/all/themes/nova/templates/defauts/page--landing-page.tpl.php
@@ -1,10 +1,10 @@
 <div class="hero" style="background-image: url('/<?php print IMG_PATH; ?>nova-hero.jpg');">
   <div class="hero__copy">
     <div class="hero__logo">
-      <div class="logo"></div>
+      <a href="/"><div class="logo"></div></a>
     </div>
     <div class="hero__title">
-      <svg class="logo-text"><use xlink:href="#logo-text"></use></svg>
+      <a href="/"><svg class="logo-text"><use xlink:href="#logo-text"></use></svg></a>
     </div>
     <div class="hero__by-line">
       <ul class="tagline">
